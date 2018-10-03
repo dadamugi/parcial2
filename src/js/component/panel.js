ca8e4a29a3d7543c0e053cfa8ee75e8656d41e78
Helder Castrillón <helder_947@hotmail.com>
2018-10-02 20:56:48 -0500

    heldercastrillon

--- src/js/component/panel.js
+++ src/js/component/panel.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../../css/panel.css';
-import imagen1 from '../../img/icono.png';
+
 class Panel extends React.Component{
     render(){
+        const style={
+            img:{
+                width:50,
+                height:50,
+                textAlign:'center'
+            }
+        }
         return(
             <div className='Panel'>
-            <img src={imagen1}/>
+            <img src={this.props.img} style={style.img}/>
             <h2>{this.props.title}</h2>
             <p>{this.props.content}</p>
             </div>
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import Panel from './component/panel.js'
+//Lista de imagenes
+
+import imagen1 from '../img/icono.png';
+import imagen2 from '../img/people.png';
+import imagen3 from '../img/world.png';
+import imagen4 from '../img/contact.png';
+//
 //ReactDOM.render("que voy a rederizar", "donde voy a rederizar");
 //const miContenido= <h1> Hola react</h1>;
 const Miapp=document.getElementById("app");
 ReactDOM.render(
     <div>
-<Panel title="Mi componente1" content="Mi contenido"/>
-<Panel title="Mi componente2" content="Mi contenido1"/>
-<Panel title="Mi componente3" content="Mi contenido2"/>
-<Panel title="Mi componente4" content="Mi contenido3"/>
-<Panel title="Mi componente4" content="Mi contenido3"/>
+<Panel title="Programación" content="Enlaces a sitios de programación" img={imagen1}/>
+<Panel title="comunidad" content="Mi contenido1" img={imagen2}/>
+<Panel title="trabajos" content="Mi contenido2" img={imagen3}/>
+<Panel title="conactos" content="Mi contenido3" img={imagen4} />
 </div>
 ,Miapp);
