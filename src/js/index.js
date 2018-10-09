a83bcfa4e7987f18919266f9af1d63645366eafd
Helder Castrillón <helder_947@hotmail.com>
2018-10-09 18:39:20 -0500

    cambios grid

--- src/js/index.js
+++ src/js/index.js
@@ -10,12 +10,26 @@ import imagen4 from '../img/contact.png';
 //
 //ReactDOM.render("que voy a rederizar", "donde voy a rederizar");
 //const miContenido= <h1> Hola react</h1>;
-const Miapp=document.getElementById("app");
+const Miapp = document.getElementById("app");
 ReactDOM.render(
     <div>
-<Panel title="Programación" content="Enlaces a sitios de programación" img={imagen1}/>
-<Panel title="comunidad" content="Mi contenido1" img={imagen2}/>
-<Panel title="trabajos" content="Mi contenido2" img={imagen3}/>
-<Panel title="conactos" content="Mi contenido3" img={imagen4} />
-</div>
-,Miapp);
+        <div class='grid-container'>
+            <div class='a'>
+                <Panel title={"Programación"} content="Enlaces a sitios de programación" img={imagen1} />
+            </div>
+            <div class='b'>
+                <Panel title="comunidad" content="Mi contenido1" img={imagen2} />
+            </div>
+            <div class='c'>
+                <Panel title="trabajos" content="Mi contenido2" img={imagen3} />
+            </div>
+            <div class='d'>
+                <Panel title="conactos" content="Mi contenido3" img={imagen4} />
+            </div>
+        </div>
+
+
+
+
+    </div>
+    , Miapp);
